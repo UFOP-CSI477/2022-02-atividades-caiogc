95a0cf11ed2fa1d45b06c207bcc585f6d301f0ca
caiogc <pcainho13@gmail.com>
2023-03-13 19:27:05 -0300

    criação da interface com vite e integração com servidor

---
+++ Atividades/exercicios/007-aplicacao/web-vite/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Menu from './components/menu/Menu'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>TESTANDO...</h1>
+      <Menu />
+    </div>
+    
+    )
+}
+
+export default App
